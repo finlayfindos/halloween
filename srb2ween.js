927778d1cb136430b3b7e54ce99a16f035855f4f
finlayfindos <nootnoot873@gmail.com>
2026-05-30 19:26:29 +0100

    Add files via upload

--- srb2ween.js
+++ srb2ween.js
@@ -74,7 +74,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\finli\AppData\Local\Temp\tmpxf090yy3.js
+// include: C:\Users\finli\AppData\Local\Temp\tmp8niuk4fj.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -202,21 +202,21 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
   })();
 
-// end include: C:\Users\finli\AppData\Local\Temp\tmpxf090yy3.js
-// include: C:\Users\finli\AppData\Local\Temp\tmpxon2gkhr.js
+// end include: C:\Users\finli\AppData\Local\Temp\tmp8niuk4fj.js
+// include: C:\Users\finli\AppData\Local\Temp\tmpr9g4kdh7.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\finli\AppData\Local\Temp\tmpxon2gkhr.js
-// include: C:\Users\finli\AppData\Local\Temp\tmpjd3otaw2.js
+  // end include: C:\Users\finli\AppData\Local\Temp\tmpr9g4kdh7.js
+// include: C:\Users\finli\AppData\Local\Temp\tmp_pccqz1y.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\finli\AppData\Local\Temp\tmpjd3otaw2.js
+  // end include: C:\Users\finli\AppData\Local\Temp\tmp_pccqz1y.js
 
 
 var arguments_ = [];
